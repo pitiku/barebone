115393618205759b3243deba89523b49e80f475b
pitiku <pitiku@gmail.com>
2026-06-01 16:57:25 +0200

    DontDestroy

--- Assets/scripts/Utils/DontDestroyOnLoad.cs
+++ Assets/scripts/Utils/DontDestroyOnLoad.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
     public string m_sTag;
 
+    private static readonly Dictionary<string, DontDestroyOnLoad> m_oInstanceMap = new Dictionary<string, DontDestroyOnLoad>();
+
     void Awake()
     {
-        if (m_sTag.isNullOrEmpty()) { return; }
-
-        GameObject[] gos = GameObject.FindGameObjectsWithTag(m_sTag);
-        if (gos.Length > 1)
+        if (m_sTag.isNullOrEmpty())
         {
-            gameObject.SetActive(false);
             Destroy(gameObject);
+            return;
         }
+
+        if (m_oInstanceMap.TryGetValue(m_sTag, out _))
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        m_oInstanceMap[m_sTag] = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    void Start()
+    void OnDestroy()
     {
-        DontDestroyOnLoad(gameObject);
+        // Unregister from tracking if this was the registered instance
+        if (!m_sTag.isNullOrEmpty() && m_oInstanceMap.TryGetValue(m_sTag, out var oInstance) && oInstance == this)
+        {
+            m_oInstanceMap.Remove(m_sTag);
+        }
     }
 }
